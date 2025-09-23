431065e4095d7a9d4d97524b2963076a43f644ef
DJAMOUS <DJAMOUS@V18H1N-DWDEV1>
2025-09-23 14:38:28 -0400

    Test

--- Form1.cs
+++ Form1.cs
@@ -18,42 +18,26 @@ namespace SecurityTest
         private void CombineIntoSinglePdf(string[] pdfFiles)
         {
 
-            // if only one PDF file exisits in this folder exist and show error message 
-
-
-            //All combined pdfs are kept in memory untill output.save() is called.
             PdfDocument output = new PdfDocument();
 
             int count = 0;
            
-            const int MB = 1024 * 1024;
-
+            
             foreach (string file in pdfFiles)
             {
-           
-                    //clean the RAM
-                    output.Close();
-                    output = null;
-                    GC.Collect();
-
                
-                    //Log("Combing canceled by user");
-                  //  return null;
                 }
                 PdfDocument input = PdfReader.Open("", PdfDocumentOpenMode.Import);
                 foreach (PdfPage page in input.Pages)
                 {
                     output.AddPage(page);
                 }
-                input.Close();
-
-                count++;
-             
+          
                 Application.DoEvents();
             }
        
             
-            //Log($"Single PDF created: {outputPath}");
+          
            
         }
 
